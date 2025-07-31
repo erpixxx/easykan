379c0eda6dc59ac57181f73b18c45cdb304d19bd
erpix <81936289+erpixxx@users.noreply.github.com>
2025-07-31 19:20:49 +0200

    refactor: change return types to Set for user project queries in UserProjectsRepository

--- server/src/main/java/dev/erpix/easykan/repository/UserProjectsRepository.java
+++ server/src/main/java/dev/erpix/easykan/repository/UserProjectsRepository.java
@@ -4,15 +4,16 @@ import dev.erpix.easykan.model.project.EKUserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface UserProjectsRepository extends JpaRepository<EKUserProject, EKUserProject.Id> {
 
-    List<EKUserProject> findByUserId(UUID userId);
+    Set<EKUserProject> findById_UserId(UUID userId);
 
-    List<EKUserProject> findByProjectId(UUID projectId);
+    Set<EKUserProject> findById_ProjectId(UUID projectId);
 
-    List<EKUserProject> findByIdUserIdAndProject_Id(UUID idUserId, UUID projectId);
+    Set<EKUserProject> findById_UserIdAndProject_Id(UUID userId, UUID projectId);
+
 }
