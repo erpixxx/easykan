8c862156c7a36800a44c665585abe5f1e41bb3fe
erpix <81936289+erpixxx@users.noreply.github.com>
2025-08-16 22:50:14 +0200

    refactor: Clean up SecurityConfig by removing unused imports

--- server/src/main/java/dev/erpix/easykan/server/config/SecurityConfig.java
+++ server/src/main/java/dev/erpix/easykan/server/config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package dev.erpix.easykan.server.config;
 
 import dev.erpix.easykan.server.config.filter.JwtAuthFilter;
-import dev.erpix.easykan.server.domain.auth.service.DiscordOAuth2UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
