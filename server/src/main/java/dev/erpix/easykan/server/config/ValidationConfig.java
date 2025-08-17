dbcd64b3196fe03a37babccd06d7d637abba4c85
erpix <81936289+erpixxx@users.noreply.github.com>
2025-08-17 23:14:20 +0200

    feat: Add ValidationConfig for email validation using Hibernate Validator

---
+++ server/src/main/java/dev/erpix/easykan/server/config/ValidationConfig.java
@@ -0,0 +1,15 @@
+package dev.erpix.easykan.server.config;
+
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValidationConfig {
+
+    @Bean
+    public EmailValidator emailValidator() {
+        return new EmailValidator();
+    }
+
+}
