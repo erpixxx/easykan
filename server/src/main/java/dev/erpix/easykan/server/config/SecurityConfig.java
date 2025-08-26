68d3ed68962991895d2dd044102ded2f0cce03bc
erpix <81936289+erpixxx@users.noreply.github.com>
2025-08-26 14:31:47 +0200

    feat: enable CORS support in security configuration

--- server/src/main/java/dev/erpix/easykan/server/config/SecurityConfig.java
+++ server/src/main/java/dev/erpix/easykan/server/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,6 +40,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated())
+                .cors(Customizer.withDefaults())
                 .exceptionHandling(handl -> handl
                         .authenticationEntryPoint((req, res, ex) ->
                                 res.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage())))
