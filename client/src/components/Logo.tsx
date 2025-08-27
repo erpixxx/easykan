62fc4fc56e9a347e7221bfcc04d7b8f023b49bcf
erpix <81936289+erpixxx@users.noreply.github.com>
2025-08-27 15:29:18 +0200

    clean: remove unused assets

--- client/src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/EasyKan.svg';
-
-export function Logo() {
-  return (
-    <svg className="ek-brand-logo" viewBox="0 0 100 100">
-      <path d={logo} />
-    </svg>
-  );
-}
