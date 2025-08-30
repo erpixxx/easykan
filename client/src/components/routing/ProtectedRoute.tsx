57f851c81965ec60ec11ce71f1480a0fc9043918
erpix <81936289+erpixxx@users.noreply.github.com>
2025-08-30 21:03:53 +0200

    refactor: Use constant path

--- client/src/components/routing/ProtectedRoute.tsx
+++ client/src/components/routing/ProtectedRoute.tsx
@@ -1,12 +1,12 @@
 import { useAuth } from '../../context/AuthContext.tsx';
 import { Navigate, Outlet } from 'react-router-dom';
-import { loginPath } from "../../routes/route.ts";
+import { LOGIN_PAGE_PATH } from "../../routes/route.ts";
 
 export function ProtectedRoute() {
   const { user } = useAuth();
 
   if (!user) {
-    return <Navigate to={loginPath} replace />;
+    return <Navigate to={LOGIN_PAGE_PATH} replace />;
   }
 
   return <Outlet />;
--- client/src/components/routing/PublicRoute.tsx
+++ client/src/components/routing/PublicRoute.tsx
@@ -1,12 +1,12 @@
 import { useAuth } from '../../context/AuthContext.tsx';
 import { Navigate, Outlet } from 'react-router-dom';
-import { defaultPath } from "../../routes/route.ts";
+import { DEFAULT_PAGE_PATH } from "../../routes/route.ts";
 
 export function PublicRoute() {
   const { user } = useAuth();
 
   if (user) {
-    return <Navigate to={defaultPath} replace />;
+    return <Navigate to={DEFAULT_PAGE_PATH} replace />;
   }
 
   return <Outlet />;
