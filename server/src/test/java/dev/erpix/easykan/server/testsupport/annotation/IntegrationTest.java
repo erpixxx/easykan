04cd1d7bc77c0b78dbfc7756b3a7fd3c5c1a0fd1
erpix <81936289+erpixxx@users.noreply.github.com>
2025-10-02 22:40:20 +0200

    feat: add TestPropertySource for JWT secret in IntegrationTest annotation

--- server/src/test/java/dev/erpix/easykan/server/testsupport/annotation/IntegrationTest.java
+++ server/src/test/java/dev/erpix/easykan/server/testsupport/annotation/IntegrationTest.java
@@ -9,12 +9,14 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @SpringBootTest
 @ActiveProfiles("prod")
 @Import(TestcontainersConfig.class)
+@TestPropertySource(properties = "easykan.jwt.secret=testsecretkeyforjwtwhichisverysecure")
 @Transactional
 public @interface IntegrationTest {
 
