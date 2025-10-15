d30106c0258eec6f58cf8f4629b5525dceb2601e
erpix <81936289+erpixxx@users.noreply.github.com>
2025-10-15 15:17:27 +0200

    feat: Allow overriding default properties in tests

--- server/src/test/java/dev/erpix/easykan/server/testsupport/config/DefaultTestProperties.java
+++ server/src/test/java/dev/erpix/easykan/server/testsupport/config/DefaultTestProperties.java
@@ -1,5 +1,6 @@
 package dev.erpix.easykan.server.testsupport.config;
 
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.TestPropertySource;
 
 import java.lang.annotation.ElementType;
@@ -7,6 +8,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to set default spring test properties for tests.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @TestPropertySource(properties = { "easykan.jwt.secret=testsecretkeyforjwtwhichisverysecure",
@@ -17,5 +21,18 @@ import java.lang.annotation.Target;
 		"easykan.server-url=http://localhost:8080", "easykan.create-default-admin-account=false",
 		"easykan.oidc.enabled=false" })
 public @interface DefaultTestProperties {
+
+	/**
+	 * Custom properties to be added or override the default ones.
+	 * @return the properties
+	 */
+	@AliasFor(annotation = TestPropertySource.class, attribute = "properties")
+	String[] value() default { "easykan.jwt.secret=testsecretkeyforjwtwhichisverysecure",
+			"easykan.jwt.access-token-expire=60", "easykan.jwt.refresh-token-expire=900",
+			"easykan.password.enabled=true", "easykan.password.min-length=8", "easykan.password.require-uppercase=true",
+			"easykan.password.require-lowercase=true", "easykan.password.require-digit=true",
+			"easykan.password.require-special-character=true", "easykan.client-url=http://localhost",
+			"easykan.server-url=http://localhost:8080", "easykan.create-default-admin-account=false",
+			"easykan.oidc.enabled=false" };
 
 }
