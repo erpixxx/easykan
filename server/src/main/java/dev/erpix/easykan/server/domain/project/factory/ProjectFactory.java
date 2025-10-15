bbc0b2a7a21531b4c502a1b20873799a2d1a81ea
erpix <81936289+erpixxx@users.noreply.github.com>
2025-10-15 15:21:44 +0200

    fix: update ProjectMember permissions to use ProjectPermission enum for clarity

--- server/src/main/java/dev/erpix/easykan/server/domain/project/factory/ProjectFactory.java
+++ server/src/main/java/dev/erpix/easykan/server/domain/project/factory/ProjectFactory.java
@@ -2,6 +2,7 @@ package dev.erpix.easykan.server.domain.project.factory;
 
 import dev.erpix.easykan.server.domain.project.model.Project;
 import dev.erpix.easykan.server.domain.project.model.ProjectMember;
+import dev.erpix.easykan.server.domain.project.model.ProjectPermission;
 import dev.erpix.easykan.server.domain.project.model.ProjectUserView;
 import dev.erpix.easykan.server.domain.user.model.User;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,11 @@ public class ProjectFactory {
 	public Project create(String name, User owner, int nextPositionForOwner) {
 		Project project = Project.builder().name(name).owner(owner).build();
 
-		ProjectMember member = ProjectMember.builder().project(project).user(owner).permissions(0L).build();
+		ProjectMember member = ProjectMember.builder()
+			.project(project)
+			.user(owner)
+			.permissions(ProjectPermission.OWNER.getValue())
+			.build();
 		project.getMembers().add(member);
 
 		ProjectUserView userView = ProjectUserView.builder()
