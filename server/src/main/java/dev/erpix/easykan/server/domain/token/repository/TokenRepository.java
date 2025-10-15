2802990d8947bab67d3d65c47b479495faaa8fdb
erpix <81936289+erpixxx@users.noreply.github.com>
2025-10-15 15:22:10 +0200

    feat: mark TokenRepository as primary bean for dependency injection

--- server/src/main/java/dev/erpix/easykan/server/domain/token/repository/TokenRepository.java
+++ server/src/main/java/dev/erpix/easykan/server/domain/token/repository/TokenRepository.java
@@ -6,11 +6,13 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+@Primary
 @Repository
 public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
 
